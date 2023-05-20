b73dd614ba924887657ebaa91ee9f668ba870eae
Assoees <132949073+Assoees@users.noreply.github.com>
2023-05-20 13:04:52 +0300

    Add files via upload

---
+++ kilpkonn15.py
@@ -0,0 +1,47 @@
+#Asso Eesmäe
+#20.05.2023
+#Python turtle
+#Kujund 15
+
+
+
+import turtle
+
+#joonistab ruudu
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+#vahetab värvi roheliseks
+turtle.color("lime")
+#keerab 60 kraadi
+turtle.left(60)
+#liigub edasi 100 ühikut
+turtle.forward(100)
+#keerab paremale 120 kraadi
+turtle.right(120)
+#liigub 100 ühikut
+turtle.forward(100)
+#vahetab värvi mustaks
+turtle.color("black")
+#keerab paremale 30 kraadi
+turtle.right(30)
+#liigub 100 ühikut
+turtle.forward(100)
+#keerab paremale 90 kraadi
+turtle.right(90)
+#liigub 25 ühikut
+turtle.forward(25)
+# vahetab värvi punaseks
+turtle.color("red")
+#keerab paremale 90 kraadi
+turtle.right(90)
+#liigub 50 ühikut
+turtle.forward(50)
+#keerab vasakule 90 kraadi
+turtle.left(90)
+#liigub 50 ühikut
+turtle.forward(50)
+#keerab vasakule 90 kraadi
+turtle.left(90)
+#liigub 50 ühikut
+turtle.forward(50)
